e4f7e676176d3a73088a107becffed8c50d9b48d
Yusuf Poyraz <128636765+yusuffpoyraz@users.noreply.github.com>
2026-04-23 09:16:50 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
-set -o errexit
+set -e
 
-# Install python dependencies
 pip install -r requirements.txt
 
-# Create a directory for ffmpeg if it doesn't exist
+# Eski dosyaları temizle ve sıfırdan kur
+rm -rf ffmpeg
 mkdir -p ffmpeg
 
-# Download and extract FFmpeg static binaries
-curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar xJ -C ffmpeg --strip-components 1
+# FFmpeg indir
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
 
-# Make sure the binary is executable
-chmod +x ffmpeg/ffmpeg
+# Dosyayı aç ve klasöre yerleştir
+tar xf ffmpeg.tar.xz -C ffmpeg --strip-components=1
+
+# Gereksiz paketi sil
+rm ffmpeg.tar.xz
+
+# Çalıştırma izni ver
+chmod +x ffmpeg/ffmpeg ffmpeg/ffprobe
