fc3dcd6cc8c88685848585ee168bfed30012d288
Yusuf Poyraz <128636765+yusuffpoyraz@users.noreply.github.com>
2026-04-23 08:20:03 +0300

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Install python dependencies
-pip install -r requirements.txt
-
-# Download and extract FFmpeg static binaries
-mkdir -p ffmpeg
-curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar xJ -C ffmpeg --strip-components=1
